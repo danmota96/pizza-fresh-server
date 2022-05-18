c806d9ad0a50750de2c3bd47634ddb35fd2a0eb9
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-18 16:19:47 -0300

    feat(table): create dto 'createtabledto'

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { CreateTableDto } from "./dto/create-table.dto";
 
 @Injectable()
 export class TableService{
+  tables =[];
+
   findAll() {
     return 'Buscar todas as mesas';
   }
-  create() {
-    return 'Criar uma mesa';
+  create(createTableDto: CreateTableDto) {
+    return 'Criar uma mesa:' + JSON.stringify(createTableDto);
   }
 }

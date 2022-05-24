6dc70d9b9e05ac7e0d9198bbeb527fc1ccba47cd
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-24 18:56:22 -0300

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,8 +1,31 @@
-export class CreateProductDto {
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString, IsUrl } from "class-validator";
 
+export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Mussarela',
+  })
+  
   name: string;
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: "Queijo mussarela, massa de ervas finas",
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 15.35,
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
+
   image: string;
 
 }
